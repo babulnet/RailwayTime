8a1155dc3da663881035ab45125369831354ac04
Babul Raj <babulsraj@gmail.com>
2019-05-02 19:52:52 +0530

    Initial Commit

---
+++ RailwayTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RailwayTime
+//
+//  Created by Babul Raj on 02/05/19.
+//  Copyright © 2019 Babul Raj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
